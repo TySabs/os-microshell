b15f947ab663ba429d20b702b7815b9daadcb04e
Ty Sabs <tysabs@gmail.com>
2018-09-25 13:40:19 -0500

    Loop shell prompt

--- hw3.cc
+++ hw3.cc
@@ -9,31 +9,37 @@
 #include <unistd.h>
 #include <string.h>
 
-const int MAX_INPUT_LENGTH = 128;
+const int MAX_INPUT_LENGTH = 1024;
 using std::cout;
 using std::endl;
 
-int main(int argc, char *argv[]) {
-  char shellPrompt[] = "480shell> ",
-        input1[MAX_INPUT_LENGTH];
-
-  int readSize,
-        quitFlag = 1;
-
+void printShellPrompt() {
+  char shellPrompt[] = "480shell> ";
   write(1, shellPrompt, strlen(shellPrompt));
-  readSize = read(0, input1, MAX_INPUT_LENGTH);
-  input1[readSize] = '\0';
+}
 
-  write(1, input1, strlen(input1));
+int main(int argc, char *argv[]) {
+  char input1[MAX_INPUT_LENGTH] = {};
+  int readSize,
+        quitFlag1 = 1,
+        quitFlag2 = 1;
 
-  // Check to make sure user did not type quit
-  quitFlag = strncmp(input1, "quit", 4);
+  while (quitFlag1 != 0 && quitFlag2 != 0) {
+    printShellPrompt();
+    readSize = read(0, input1, MAX_INPUT_LENGTH);
+    input1[readSize] = '\0';
 
-  if (quitFlag == 0) {
-    cout << "Quitting the program..." << endl;
+    // Check to make sure user did not type quit
+    quitFlag1 = strncmp(input1, "quit", 4);
+    quitFlag2 = strncmp(input1, "q", 1);
+
+    if (quitFlag1 == 0 || quitFlag2 == 0) {
+      cout << "Quitting the program..." << endl;
+      exit(0);
+    }
+
+    system(input1);
   }
-
-  system(input1);
 
   write(1, "\n", 1);
   return 0;
