a02ad4494a3c68a6342a99e47532113357840937
Ty Sabs <tysabs@gmail.com>
2018-09-27 10:40:47 -0500

    Tokenize with a list, thus fixing execvp arguments

--- hw3.cc
+++ hw3.cc
@@ -8,25 +8,29 @@
 #include <iostream>
 #include <unistd.h>
 #include <string.h>
+#include <list>
+#include <algorithm>
 
 const int MAX_INPUT_LENGTH = 1024;
-using std::cout;
+using std::cerr;
 using std::endl;
+using std::list;
+using std::copy;
 
 void printShellPrompt() {
   char shellPrompt[] = "480shell> ";
   write(1, shellPrompt, strlen(shellPrompt));
 }
 
-bool checkForQuit(char *input1) {
+bool checkForQuit(char *input) {
   int quitFlag1 = 1,
       quitFlag2 = 1;
   // Check to make sure user did not type quit
-  quitFlag1 = strncmp(input1, "quit", 4);
-  quitFlag2 = strncmp(input1, "q", 1);
+  quitFlag1 = strncmp(input, "quit", 4);
+  quitFlag2 = strncmp(input, "q", 1);
 
   if (quitFlag1 == 0 || quitFlag2 == 0) {
-    cout << "Quitting the program..." << endl;
+    cerr << "Quitting the program..." << endl;
     return false;
   } else {
     return true;
@@ -34,20 +38,63 @@ bool checkForQuit(char *input1) {
 }
 
 int main(int argc, char *argv[]) {
-  char input1[MAX_INPUT_LENGTH] = {};
-  int readSize;
+  char input[MAX_INPUT_LENGTH] = {};
+  char *token1, *cmd1;
+  list<char*> argList;
+  int readSize, status;
   bool isLooping = true;
 
   pid_t pid1;
 
-  while (isLooping == true) {
+  while (isLooping) {
     printShellPrompt();
-    readSize = read(0, input1, MAX_INPUT_LENGTH);
-    input1[readSize] = '\0';
+    readSize = read(0, input, MAX_INPUT_LENGTH);
+    input[readSize - 1] = '\0';
 
-    isLooping = checkForQuit(input1);
+    isLooping = checkForQuit(input);
+    if (isLooping == false) break;
 
-    system(input1);
+    // Fork into Parent/Child
+    if ((pid1 = fork()) < 0) {
+      cerr << "Fork Error!" << endl;
+    }
+
+    // In child process
+    if (pid1 == 0) {
+      cerr << "In child process" << endl;
+
+      token1 = strtok(input, " ");
+      cmd1 = token1;
+
+      argList.push_back(token1);
+
+      token1 = strtok(NULL, " ");
+
+      while (token1 != NULL) {
+        argList.push_back(token1);
+        token1 = strtok(NULL, " ");
+      }
+
+      char *args1[argList.size()];
+      copy(argList.begin(), argList.end(), args1);
+
+      cerr << "Command: " << cmd1 << endl;
+      for (unsigned int j = 0; j < argList.size(); j++) {
+        cerr << "Arg#" << j << ": " << args1[j] << endl;
+      }
+
+      execvp(cmd1, args1);
+
+      cerr << "exec error!" << endl;
+      exit(127);
+
+    // In parent process
+    } else {
+      if ((pid1 = waitpid(pid1, &status, 0)) < 0) {
+        cerr << "waitpid error!" << endl;
+      }
+    }
+
   } // end while loop
 
   write(1, "\n", 1);
